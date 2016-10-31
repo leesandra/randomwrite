ff25cf58fbf10a889240c6de94640f37b138ea07
leesandra <leesandra.alexandre@gmail.com>
2016-10-31 19:37:50 -0400

    first commit

---
+++ Pseudo Code.js
@@ -0,0 +1,76 @@
+
+Pseudo Code Lesson 
+
+Similar 
+height 
+weight 
+number arms 
+number legs
+
+
+difference
+gender 
+skin color 
+accessories 
+name
+age 
+
+Object Oriented Programming 
+
+DRY= DONT REPEAT YOURSELF
+
+functions are instructions***
+
+var (variables)
+
+var heightOfLif = "50px";
+
+
+
+
+// running object 
+function makeObjectRun (start, destination, speed) { }
+ liftBodyPart ();
+ moveBodyPart ();
+ setBodyPartDown (); 
+
+ function makeObjectWalk (start, destination, speed)
+
+// object lifts body part
+function liftbodypart (bodypart, height) {} 
+
+// object moves body part
+funtion moveBodyPart (distance) {}
+
+// object sets body part down
+  function setBodypartDown () {}
+
+
+
+
+
+
+
+function motherMayI (stopsign) {
+if (stopsign == "green")
+}
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
